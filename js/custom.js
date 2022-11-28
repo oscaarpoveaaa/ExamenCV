c0ba1f80294159b816d7cc5fcf598dc64b9d8c40
Oscar Povea <oscaarpoveaa@gmail.com>
2022-11-28 23:09:55 +0100

    3

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,6 @@
 "use strict";
 
-function validarNumero(valor){ 
-    //intento convertir a entero. 
-    //si era un entero no le afecta, si no lo era lo intenta convertir 
-    valor = parseInt(valor)
-
-    //Compruebo si es un valor numérico 
-    if (isNaN(valor)) { 
-       //entonces (no es numero) devuelvo el valor cadena vacia 
-       return "" 
-    }else{ 
-       //En caso contrario (Si era un número) devuelvo el valor 
-       return valor 
-    } 
+function enviar(){ 
+    alert('Se ha enviado correctamente')
 }
 
-function compruebaNumero(){ 
-   	numeroValidado = validarNumero(document.f1.numero.value) 
-   	if (numeroValidado == ""){ 
-      	//si era la cadena vacía es que no era válido. Lo aviso 
-      	alert ("Debe escribir un numero válido!") 
-      	//selecciono el texto 
-      	document.f1.numero.select() 
-      	//coloco otra vez el foco 
-      	document.f1.numero.focus() 
-   	}else 
-      	document.f1.numero.value = numeroValidado 
-} 
